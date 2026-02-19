9e991d157d169dbfddf35b7ea9355e8beac19463
Tammy Solomon <tamboconsultingllc@gmail.com>
2026-02-18 23:56:01 -0500

    Rename settings-client to settings-page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/db";
-import SettingsClient from "./settings-client";
+import SettingsClient from "./settings-page";
 
 export default async function SettingsPage() {
   const { userId } = await auth();
